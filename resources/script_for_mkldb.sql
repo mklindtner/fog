40f61c8222786607cda61eda79b31f41d5af91f7
mkl <mkl@mikiaholding.dk>
2018-06-01 23:20:39 +0200

    updated sqlScript

--- resources/script_for_mkldb.sql
+++ resources/script_for_mkldb.sql
@@ -1,3 +1,5 @@
+# noinspection SqlNoDataSourceInspectionForFile
+
 SET @@global.time_zone = '+01:00';
 DROP DATABASE IF EXISTS `fogdb`;
 CREATE DATABASE IF NOT EXISTS `fogdb`;
@@ -21,62 +23,64 @@ CREATE TABLE roles (
 
 CREATE TABLE employees(
      id int(6) UNSIGNED auto_increment PRIMARY KEY,
-    `username` varchar(30) NOT NULL UNIQUE,
-    `password` varchar(30) NOT NULL, 
+    `username` varchar(80) NOT NULL UNIQUE,
+    `password` varchar(80) NOT NULL,
     `phone` int unsigned,
     `role_id` int(6) UNSIGNED NOT NULL,
     `reg_date` TIMESTAMP NOT NULL,
-     FOREIGN KEY (`role_id`) REFERENCES roles(id) 
+     FOREIGN KEY (`role_id`) REFERENCES roles(id)
 );
 
 
-CREATE TABLE materials ( 
+CREATE TABLE materials (
     id int(6) unsigned auto_increment PRIMARY KEY,
     pricePrUnit int NOT NULL,
     description varchar(300) default "None" NOT NULL
-);
-
-create table orderLine (
-    id int(6) unsigned auto_increment PRIMARY KEY,
-    amount int NOT NULL,
-    unit varchar(30) NOT NULL,
-    description varchar(300),
-    orderId int(6) unsigned,
-    materialId int(6) unsigned, 
-    foreign key(orderId) REFERENCES orders(id),
-    foreign key(materialId) REFERENCES materials(id)
 );
 
 CREATE TABLE sheds (
     id int UNSIGNED NOT NULL auto_increment PRIMARY KEY,
     length int UNSIGNED NOT NULL,
     width int UNSIGNED NOT NULL,
-    height int UNSIGNED NOT NULL, 
     hasFloor boolean
 );
 
 
 CREATE TABLE orders (
     id int(6) unsigned NOT NULL auto_increment PRIMARY KEY,
-    height int unsigned NOT NULL, 
+    height int unsigned NOT NULL,
     width int unsigned NOT NULL,
-    length int unsigned NOT NULL,    
-    `status` enum('AVAILABLE', 'TAKEN', 'SEND', 'ACCEPTED'),    
-    slope int unsigned NOT NULL,            
+    length int unsigned NOT NULL,
+    slope int unsigned NOT NULL,
+    `status` enum('PENDING','OFFER','ACCEPTED','SEND'),
     price int,
     created_at TIMESTAMP,
     shedId int(6) unsigned,
     customerId int(6) unsigned NOT NULL,
-    `employeeId` int(6) unsigned, 
-    FOREIGN KEY (customerId) 
-		REFERENCES customers(id) 
+    `employeeId` int(6) unsigned,
+    FOREIGN KEY (customerId)
+		REFERENCES customers(id)
         ON DELETE CASCADE,
-    FOREIGN KEY (shedId) 
-		REFERENCES sheds(id) 
+    FOREIGN KEY (shedId)
+		REFERENCES sheds(id)
         ON DELETE CASCADE,
-    FOREIGN KEY (`employeeId`) 
+    FOREIGN KEY (`employeeId`)
 		REFERENCES employees(id)
         ON DELETE CASCADE
+);
+
+
+create table orderLines (
+    id int(6) unsigned auto_increment PRIMARY KEY,
+    amount int NOT NULL,
+    unit varchar(30) NOT NULL,
+    description varchar(300),
+    length int,
+    isTreeOrRoof boolean,
+    orderId int(6) unsigned,
+    materialId int(6) unsigned,
+    foreign key(orderId) REFERENCES orders(id),
+    foreign key(materialId) REFERENCES materials(id)
 );
 
 
@@ -108,15 +112,15 @@ Insert Into materials(description, pricePrUnit) VALUES ("vinkelbeslag 35", 280);
 
 
 
-Insert Into employees(username, password, phone, role_id, reg_date) VALUES("emp@compnayEmail.com", "123", 1234567, 3, now());
-Insert into employees(username, password, phone, role_id, reg_date) VALUES ("empTest", "123", 123456, 3, now());
+Insert Into employees(username, password, phone, role_id, reg_date) VALUES("employeeResp@compnayEmail.com", "123", 1234567, 2, now());
+Insert into employees(username, password, phone, role_id, reg_date) VALUES ("employeeWorker@companyEmail.com", "123", 123456, 3, now());
+Insert into employees(username, password, phone, role_id, reg_date) VALUES ("Martin@companyEmail.com", "123", 123456, 1, now());
 
-Insert Into customers(username, password, reg_date, phone) VALUES ("customer", "123", now(), 123456);
+Insert Into customers(username, password, reg_date, phone) VALUES ("peter@hotmail.com", "123", now(), 123456);
 
-Insert Into orders(height, width, `length`, slope, customerId, created_at, employeeId, `status`) VALUES (480, 670, 840, 45, 1, now(), 2, "PENDING"); 
-Insert Into orders(height, width, `length`, slope, customerId, created_at, employeeId, `status`) VALUES (980, 780, 690, 45, 1, now(), 2, "PENDING"); 
-Insert Into orders(height, width, `length`, slope, customerId, created_at, employeeId, `status`) VALUES (980, 670, 540, 45, 1, now(), 2, "PENDING"); 
-
+Insert Into orders(height, width, `length`, slope, customerId, created_at, employeeId, `status`) VALUES (480, 670, 840, 45, 1, now(), null, "PENDING");
+Insert Into orders(height, width, `length`, slope, customerId, created_at, employeeId, `status`) VALUES (980, 780, 690, 45, 1, now(), null, "PENDING");
+Insert Into orders(height, width, `length`, slope, customerId, created_at, employeeId, `status`) VALUES (980, 670, 540, 45, 1, now(), null, "PENDING");
 
 
 
